40ba8b42db06f03fa655c2a6dad73ecf3a80a1ee
gurjao <gurjao@gmail.com>
2024-12-17 18:12:52 -0300

    Adjust gitignore and push pom.xml and src/ first time

---
+++ backend/src/main/java/com/qimatech/QimatechApplication.java
@@ -0,0 +1,13 @@
+package com.qimatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QimatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QimatechApplication.class, args);
+	}
+
+}
